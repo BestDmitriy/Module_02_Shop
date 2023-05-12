c4b0ef75248018f91574c42fd2f6addf1772ae75
BestDmitriy <Reshetnikov.111@mail.ru>
2023-05-12 09:16:59 +0300

    add all

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './index.html'; /*либо './images/logo.png';*/
 import './styles/main.scss'; 
 
 import Swiper, { Navigation, Pagination } from 'swiper'; 
+
 var swiper = new Swiper(".offerSwiper", {
     slidesPerView: 3,
     spaceBetween: 30,
@@ -21,6 +22,20 @@ var swiper = new Swiper(".offerSwiper", {
       prevEl: ".swiper-button-prev",
     },
     modules: [Navigation, Pagination],
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1
+      },
+      768: {
+          slidesPerView: 2,
+          slidesPerGroup: 2
+      },
+      1000: {
+          slidesPerView: 3,
+          slidesPerGroup: 3
+      }
+    }
   });
 
   var swiper = new Swiper(".trendsSwiper", {
@@ -38,4 +53,27 @@ var swiper = new Swiper(".offerSwiper", {
       prevEl: ".swiper-button-prev",
     },
     modules: [Navigation, Pagination],
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1
+      },
+      768: {
+          slidesPerView: 2,
+          slidesPerGroup: 2
+      },
+      1200: {
+          slidesPerView: 3,
+          slidesPerGroup: 3
+      }
+    }
   });
+
+  var swiper = new Swiper(".categorySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
